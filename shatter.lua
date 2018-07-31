03ada9f432f87fab944e595a28fe3d4fb97d674e
hugeblank <toddlangejr@gmail.com>
2018-07-30 21:50:13 -0700

    Update shatter.lua
    - changed how the cursor object is created
    - added setTextScale for values from .5 to 10, rejoice!
    - debugged rendering so that background gets fully rendered, and then text. Keeps text characters from getting clipped by the background when it smaller terminal modes.
    - add "shatter_resize" event

--- shatter.lua
+++ shatter.lua
@@ -10,8 +10,10 @@ can.clear()
 local colors = {white = 0xf0f0f0ff, orange = 0xf2b233ff, magenta = 0xe57fd8ff, lightBlue = 0x99b2f2ff, yellow = 0xdede6cff, lime = 0x7fcc19ff, pink = 0xf2b2ccff, gray = 0x4c4c4cff, lightGray = 0x999999ff, cyan = 0x4c99b2ff, purple = 0xb266e5ff, blue = 0x3366ccff, brown = 0x7f664cff, green = 0x57a64eff, red = 0xcc4c4cff, black = 0x191919ff}
 --colors by number
 local cbn = {colors.white, colors.orange, colors.magenta, colors.lightBlue, colors.yellow, colors.lime, colors.pink, colors.gray, colors.lightGray, colors.cyan, colors.purple, colors.blue, colors.brown, colors.green, colors.red, colors.black}
---term scale
+--default term scale
 local ox, oy = 6, 9
+-- default term scale
+local sx, sy = 6, 9
 --term bg and fg colors
 local bg, fg, bgbn, fgbn = colors.black, colors.white, 2^(#cbn-1), 2^0
 --cursor, pos, and blink
@@ -24,7 +26,7 @@ tx, ty = math.floor(tx/ox), math.floor(ty/oy)
 --screen rendering in a table
 local screen = {}
 --populate that table
-do
+local function tPop()
     local x, y = can.getSize()
     for i = 0, x, ox do
         screen[#screen+1] = {}
@@ -33,6 +35,7 @@ do
         end
     end
 end
+tPop()
 function handler()
     active = true
     os.queueEvent("shatter_handler")
@@ -42,12 +45,13 @@ function handler()
         while true do
             if not cb then
                 csr.setText(" ")
+                sleep()
             else
                 csr.setText("_")
                 sleep(.4)
                 csr.setText(" ")
+                sleep(.4)
             end
-            sleep(.4)
         end
     end,
     function()
@@ -70,10 +74,11 @@ local function write(x, y, char, color)
     x, y = math.floor(x), math.floor(y)
     if x > 0 and y > 0 and x <= tx and y <= ty then
         if screen[x][y].fg.getColor == nil then
-            screen[x][y].fg = can.addText({((x-1)*ox)+1, ((y-1)*oy)+1}, char, color)
+            screen[x][y].fg = can.addText({((x-1)*ox)+1, ((y-1)*oy)+1}, char, color, ox/sx)
         else
             screen[x][y].fg.setColor(color)
             screen[x][y].fg.setText(char)
+            screen[x][y].fg.setScale(ox/sx)
         end
     end
 end
@@ -90,15 +95,24 @@ local function draw(x, y, color)
 end
 --get the data of a particular pixel
 local function getData(pixel)
-    return {bgc = pixel.bg.getColor(),
-        fgc = pixel.fg.getColor(),
-        txt = pixel.fg.getText()}
+    if pixel.bg.getColor == nil then
+        print(textutils.serialise(pixel))
+    end
+    if pixel then
+        return {bgc = pixel.bg.getColor(),
+            fgc = pixel.fg.getColor(),
+            txt = pixel.fg.getText()}
+    end
 end
 --set the data of a particular pixel
 local function setData(pixel, data)
-    local x, y = pixel.bg.getPosition()
-    draw((x/ox)+1, (y/oy)+1, data.bgc)
-    write((x/ox)+1, (y/oy)+1, data.txt, data.fgc)
+    if pixel and data then
+        if pixel.bg.getPosition then
+            local x, y = pixel.bg.getPosition()
+            draw((x/ox)+1, (y/oy)+1, data.bgc)
+            write((x/ox)+1, (y/oy)+1, data.txt, data.fgc)
+        end
+    end
 end
 --move a row to an entirely different line
 local function move(line, to)
@@ -115,7 +129,11 @@ local function repopulate()
     for i = 1, math.floor(x/ox) do
         for j = 1, math.floor(y/oy) do
             draw(i, j, bg)
-            write(i, j, " ", fg)
+        end
+    end
+    for i = 1, math.floor(x/ox) do
+        for j = 1, math.floor(y/oy) do
+            write(i, j, "", fg)
         end
     end
 end
@@ -125,7 +143,7 @@ function getTerm()
         error("cursor handler is not initialized", 0)
     end
     repopulate()
-    csr = can.addText({cx*ox, (cy*oy)+1}, "", 0xf0f0f0ff)
+    csr = can.addText({cx*ox, (cy*oy)+1}, "", 0xffffffff, ox/sx)
     os.queueEvent("shatter_redirect")
     return out
 end
@@ -196,7 +214,7 @@ out.setCursorBlink = function(b)
 end
 out.isColor = function()
 --term.isColor
-    return true, "and 16.8 million at that!"
+    return true, "now with more alpha!"
 end
 out.isColour = out.isColor
 out.getSize = function()
@@ -206,13 +224,8 @@ end
 out.scroll = function(amount)
 --term.scroll
     local tcx, tcy = out.getCursorPos()
-    local function swoop(i, amt)
-        out.setCursorPos(1, amt)
-        out.clearLine()
-        move(i, amt)
-    end
     if type(amount) ~= "number" then
-        error("invalid argument #1 (expected numbe, got "..type(amount)..")", 2)
+        error("bad argument #1 (expected number, got "..type(amount)..")", 2)
     end
     if amount > 0 then
         for i = 1, tx do
@@ -235,12 +248,13 @@ local function invCol(col)
     error("invalid color (got "..col..")", 2)
 end
 local function lb2(num)
+--very basic implementation of base 2 logarithm
     return math.log(num)/math.log(2)
 end
 out.setTextColor = function(col)
 --term.setTextColor
     if type(col) ~= "number" then
-        error("invalid argument #1 (number expected, got "..type(col)..")", 2)
+        error("bad argument #1 (number expected, got "..type(col)..")", 2)
     end
     if lb2(col) > #cbn or lb2(col) ~= math.ceil(lb2(col)) then
         invCol(col)
@@ -252,7 +266,7 @@ end
 out.setBackgroundColor = function(col)
 --term.setBackgroundColor
     if type(col) ~= "number" then
-        error("invalid argument #1 (expected number, got "..type(col)..")", 2)
+        error("bad argument #1 (expected number, got "..type(col)..")", 2)
     end
     if lb2(col) > #cbn or lb2(col) ~= math.ceil(lb2(col)) then
         invCol(col)
@@ -278,8 +292,9 @@ local function torgba(hex)
     return vals[4]/255, vals[3]/255, vals[2]/255
 end
 out.getAlpha = function(col)
+--get the alpha value of a specific color
     if type(col) ~= "number" then
-        error("invalid argument #1 (expected number, got "..type(col)..")")
+        error("bad argument #1 (expected number, got "..type(col)..")")
     end
     if lb2(col) > #cbn or lb2(col) ~= math.ceil(lb2(col)) then
         invCol(col)
@@ -288,11 +303,12 @@ out.getAlpha = function(col)
     return  (cbn[lb2(col)+1]-(((c[1]*255)*(16^6))+((c[2]*255)*(16^4))+((c[3]*255)*(16^2))))/255
 end
 out.setAlpha = function(col, val)
+--set the alpha value of a specific color
     if type(col) ~= "number" then
-        error("invalid argument #1 (number expected, got "..type(col)..")", 2)
+        error("bad argument #1 (number expected, got "..type(col)..")", 2)
     end
     if type(val) ~= "number" then
-        error("invalid argument #2 (number expected, got "..type(val)..")", 2)
+        error("bad argument #2 (number expected, got "..type(val)..")", 2)
     end
     if lb2(col) > #cbn or lb2(col) ~= math.ceil(lb2(col)) then
         invCol(col)
@@ -307,8 +323,9 @@ out.setAlpha = function(col, val)
     end
 end
 out.getPaletteColor = function(col)
+--term.getPaletteColor
     if type(col) ~= "number" then
-        error("invalid argument #1 (number expected, got "..type(col)..")", 2)
+        error("bad argument #1 (number expected, got "..type(col)..")", 2)
     end
     if lb2(col) > #cbn or lb2(col) ~= math.ceil(lb2(col)) then
         invCol(col)
@@ -316,22 +333,22 @@ out.getPaletteColor = function(col)
     return torgba(cbn[lb2(col)+1])
 end
 out.setPaletteColor = function(cnum, r, g, b)
+--term.setPaletteColor
     if type(cnum) ~= "number" then
-        error("invalid argument #1 (number expected, god "..type(cnum)..")", 2)
+        error("bad argument #1 (number expected, got "..type(cnum)..")", 2)
     end
     if type(r) ~= "number" then
-        error("invalid argument #2 (number expected, got "..type(r)..")", 2)
+        error("bad argument #2 (number expected, got "..type(r)..")", 2)
     end
     if g then
         if type(g) ~= "number" then
-            error("invalid argument #3 (number expected, got "..type(g)..")", 2)
+            error("bad argument #3 (number expected, got "..type(g)..")", 2)
         elseif type(b) ~= "number" then
-            error("invalid argument #4 (number expected, got "..type(b)..")", 2)
+            error("bad argument #4 (number expected, got "..type(b)..")", 2)
         end
         if r > 1 then r = 1 end
         if g > 1 then g = 1 end
         if b > 1 then b = 1 end
---        print(r*255, g*255, b*255, ((((r*255)*(16^6))+((g*255)*(16^4))+((b*255)*(16^2)))*256)+255, cbn[lb2(cnum)+1])
         cbn[lb2(cnum)+1] = (((r*255)*(16^6))+((g*255)*(16^4))+((b*255)*(16^2)))+math.ceil(out.getAlpha(cnum)*255)
     else
         cbn[lb2(cnum)+1] = (r*256)+math.ceil(out.getAlpha(cnum)*255)
@@ -342,9 +359,46 @@ out.setPaletteColor = function(cnum, r, g, b)
         out.setTextColor(fgbn)
     end
 end
+--compat for all those UK'ers
 out.setTextColour = out.setTextColor
 out.setBackgroundColour = out.setBackgroundColor
 out.setPaletteColour = out.setPaletteColor
 out.getTextColour = out.getTextColor
 out.getBackgroundColour = out.getBackgroundColor
 out.getPaletteColour = out.getPaletteColor
+
+out.setTextScale = function(scale)
+-- consider reimpl, is shit
+    if type(scale) ~= "number" then
+        error("bad argument #1 (number expected, got "..type(scale)..")", 2)
+    end
+    if 0.4 >= scale or scale > 10 then
+        error("Expected number in range 0.5-10", 2)
+    end
+    ox, oy = math.ceil(scale*sx), math.ceil(scale*sy)
+    tx, ty = can.getSize()
+    tx, ty = math.floor(tx/ox), math.floor(ty/oy)
+    csr.remove()
+    local oldscr = screen -- replicate the screen
+    screen = {} -- remove it for repopulation of table w/ new scale
+    tPop() -- repopulate table
+    repopulate() -- add objects
+    csr = can.addText({cx*ox, (cy*oy)+1}, "", 0xffffffff, scale) --recreate cursor
+    for i = 1, #oldscr do --rerender screen in new scale
+        for j = 1, #oldscr[i] do
+            if oldscr[i][j].bg.getColor ~= nil then
+                if screen[i] and screen[i][j] then
+                    setData(screen[i][j], getData(oldscr[i][j]))
+                end
+                oldscr[i][j].bg.remove()
+                oldscr[i][j].fg.remove()
+            end
+        end
+    end
+    os.queueEvent("shatter_resize")
+end
+
+-- Changes to cursor creation
+-- Added setTextScale, and manipulated functions used in combination with it.
+-- Changed rendering so that entire background gets rendered before characters
+-- so that text doesn't get clipped by it.
